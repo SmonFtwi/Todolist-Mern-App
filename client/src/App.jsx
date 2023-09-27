3da68d9d8aa3e327d6513f91b24bc577b866c3d2
SmonFtwi <smonftwi1@gmail.com>
2023-09-27 16:42:12 +0400

    remove the image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './component/header'
 import './App.css'
-import img from './assets/Capture.png'
+//import img from './assets/Capture.png'
 
 
 function App(){
@@ -11,7 +11,7 @@ function App(){
       <div className='homeContent'>
         
         <h2>Start decluttering your life today. Your to-do list, your way.</h2>
-        <img src={img}></img>
+        {/* <img src={img}></img> */}
         
         <h3>Join thousands of satisfied users who trust us to keep them organized and productive.</h3>
       </div>
